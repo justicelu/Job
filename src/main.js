43982642501763417fca1d6e90fdc3fe15189233
justicelu <lu488383291@outlook.com>
2021-05-25 18:51:19 +0800

    feat: 网页全屏

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// 导入基础样式
+import '@/assets/css/base.css'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">home</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(192, 226, 226);
+}
+</style>
